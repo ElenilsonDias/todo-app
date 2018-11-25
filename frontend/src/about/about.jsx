430c951614b314a8a848cdc7c7963406cda2a6c0
ElenilsonDias <32115396+ElenilsonDias@users.noreply.github.com>
2018-11-25 00:33:46 -0200

    20181125_0033

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>
-            Sobre
-        </h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Quem habla mucho, says good morning to horse</p>
+        <h2>Missão e Visão</h2>
+        <p>Aprender e desenvolver...</p>
     </div>
 )
